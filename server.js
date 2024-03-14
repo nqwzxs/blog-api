961534c1ebe851133f045cc08356b8d8d485b748
nqwzxs <127120589+nqwzxs@users.noreply.github.com>
2024-03-14 19:24:54 +0500

    install mongoose

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require("express")
+const mongoose = require("mongoose")
 
 const app = express()
+
+const mongodb = process.env.MONGODB_URI
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongodb);
+}
 
 app.listen(3000)
