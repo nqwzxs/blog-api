58c65f57e48ffcb5ba659ca2d0e4e6a61b8265f2
nqwzxs <127120589+nqwzxs@users.noreply.github.com>
2024-04-24 18:18:38 +0500

    add post into comment schema

--- models/comment.js
+++ models/comment.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const commentSchema = new Schema({
   body: { type: String, required: true },
   author: { type: String, required: true },
+  post: { type: Schema.Types.ObjectId, ref: "Post" },
   date_created: { type: Date, default: Date.now },
 });
 
